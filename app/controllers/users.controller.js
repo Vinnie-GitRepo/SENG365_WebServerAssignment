0aeaa835da509363c126e139d43d584c3bfe5548
Vinnie <vjj14@uclive.ac.nz>
2021-03-30 00:30:39 +1300

    Testing API test server

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -1,5 +1,5 @@
-const user = require('../models/users.model');
-const passwordHelper = require('../middleware/password.middleware');
+const user = require("../models/users.model");
+const passwordHelper = require("../middleware/password.middleware");
 
 /**
  *
@@ -7,121 +7,137 @@ const passwordHelper = require('../middleware/password.middleware');
  * @param res
  * @returns 201 {"userId": (id)}, 400, 500
  */
-exports.register = async function(req, res) {
-    console.log("\nRequest to register a new user...");
+exports.register = async function (req, res) {
+  console.log("\nRequest to register a new user...");
 
-    try {
-        const user_id = await user.create(
-            req.body.firstName,
-            req.body.lastName,
-            req.body.email,
-            req.body.password);
+  try {
+    const user_id = await user.create(
+      req.body.firstName,
+      req.body.lastName,
+      req.body.email,
+      req.body.password
+    );
 
-        if (user_id === null) {
-            res.status(400).send();
-        } else {
-            console.log(user_id);
-            res.status(201).send({"userId": user_id});
-        }
-    } catch (err) {
-        res.status(500).send();
-        throw err;
+    if (user_id === null) {
+      res.status(400).send();
+    } else {
+      console.log(user_id);
+      res.status(201).send({ userId: user_id });
     }
+  } catch (err) {
+    res.status(500).send();
+    throw err;
+  }
 };
-
 
 /**
  * @param req
  * @param res
  * @returns 200 {"userId": (id), "token": (token)}, 400, 500
  */
-exports.login = async function(req, res) {
-    console.log("\nRequest to log in an existing user...");
+exports.login = async function (req, res) {
+  console.log("\nRequest to log in an existing user...");
 
-    try {
-
-        const currentUser = await user.findByEmail(req.body.email);
-        // console.log(req.body)
-        // console.log(currentUser);
-        if (currentUser === null) {
-            console.log("ERROR: currentUser is null");
-            res.status(400).send();
-        }
-
-        const correctPassword = await passwordHelper.compare(req.body.password, currentUser[0].password);
-        if (!correctPassword) {
-            console.log("ERROR: incorrect password");
-            res.status(400).send();
-        }
-
-        const result = await user.setAuthToken(currentUser[0].id);
-        if (result === null) {
-            console.log("ERROR: auth_token issue");
-            res.status(400).send();
-        }
-
-        res.status(200).send({
-            "userId": currentUser[0].id,
-            "token": result[1]
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(400).send();
+  try {
+    const currentUser = await user.findByEmail(req.body.email);
+    // console.log(req.body)
+    // console.log(currentUser);
+    if (currentUser === null) {
+      console.log("ERROR: currentUser is null");
+      res.status(400).send();
     }
-    res.status(500).send();
-};
 
+    const correctPassword = await passwordHelper.compare(
+      req.body.password,
+      currentUser[0].password
+    );
+    if (!correctPassword) {
+      console.log("ERROR: incorrect password");
+      res.status(400).send();
+    }
+
+    const result = await user.setAuthToken(currentUser[0].id);
+    if (result === null) {
+      console.log("ERROR: auth_token issue");
+      res.status(400).send();
+    }
+
+    res.status(200).send({
+      userId: currentUser[0].id,
+      token: result[1],
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).send();
+  }
+  res.status(500).send();
+};
 
 /**
  * @param req
  * @param res
  * @returns 200, 401, 500
  */
-exports.logout = async function(req, res) {
-    console.log("\nRequest to log out a currently authorised user...");
+exports.logout = async function (req, res) {
+  console.log("\nRequest to log out a currently authorised user...");
 
-    const token = req.headers["x-authorization"];
+  const token = req.headers["x-authorization"];
 
-    const authorizedUserId = await user.findByToken(token);
+  const authorizedUserId = await user.findByToken(token);
 
-    if (authorizedUserId === null) {
-        res.status(401).send();
-    }
+  if (authorizedUserId === null) {
+    res.status(401).send();
+  }
 
-    try {
-        await user.logout(authorizedUserId)
-        res.status(200).send();
-    } catch (err) {
-        console.log(err)
-        res.status(500).send();
-    }
+  try {
+    await user.logout(authorizedUserId);
+    res.status(200).send();
+  } catch (err) {
+    console.log(err);
+    res.status(500).send();
+  }
 };
-
 
 /**
  * @param req
  * @param res
  * @returns 200 {"firstName": (fname), ""},
  */
-exports.read = async function(req, res) {
-    console.log("\nRequest to retrieve information about a user...");
+exports.read = async function (req, res) {
+  console.log("\nRequest to retrieve information about a user...");
 
-    const userId = req.params.user_id;
+  const userId = req.params.user_id;
 
-    try {
-        const userData = await user.getUserById(userId);
-        if (userData === null || userData.length === 0) {
-            res.status(404).send();
-        }
-        res.status(200).send(userData);
-    } catch (err) {
-        res.status(500).send();
+  const token = req.headers["x-authorization"];
+  const authorizedUserId = await user.findByToken(token);
+
+  if (authorizedUserId === userId) {
+  }
+
+  try {
+    if (authorizedUserId === userId) {
+      const userData = await user.getUserById(userId, true);
+      if (userData === null || userData.length === 0) {
+        res.status(404).send();
+      }
+      res.status(200).send(userData);
+    } else {
+      const userData = await user.getUserById(userId);
+      if (userData === null || userData.length === 0) {
+        res.status(404).send();
+      }
+      res.status(200).send(userData);
     }
-};
-
-exports.update = async function(req, res) {
-    console.log("\nRequest to change a user's details...");
+    // if (userData === null || userData.length === 0) {
+    //   res.status(404).send();
+    // }
+    // res.status(200).send(userData);
+  } catch (err) {
     res.status(500).send();
-
+  }
 };
 
+exports.update = async function (req, res) {
+  console.log("\nRequest to change a user's details...");
+  res.status(500).send();
+};
--- app/models/users.model.js
+++ app/models/users.model.js
@@ -1,130 +1,138 @@
-const db = require('../../config/db');
-const passwordHelper = require('../middleware/password.middleware');
-const tokenGenerator = require('rand-token');
+const db = require("../../config/db");
+const passwordHelper = require("../middleware/password.middleware");
+const tokenGenerator = require("rand-token");
 
-exports.create = async function(firstName, lastName, email, password) {
+exports.create = async function (firstName, lastName, email, password) {
+  const queryString =
+    "INSERT INTO user (first_name, last_name, email, password) VALUES (?, ?, ?, ?)";
 
-    const queryString = 'INSERT INTO user (first_name, last_name, email, password) VALUES (?, ?, ?, ?)';
-
-    try {
-
-        // Crazy regex from https://emailregex.com/
-        if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
-            console.log("ERROR: email invalid");
-            return null;
-        }
-
-        const values = [
-            firstName,
-            lastName,
-            email,
-            await passwordHelper.hashPassword(password)
-        ];
-
-        const result = await db.getPool().query(queryString, values);
-        console.log(result);
-        return result[0].insertId;
-    } catch (err) {
-        console.log("ERROR: Duplicate email");
-        return null;
+  try {
+    // Crazy regex from https://emailregex.com/
+    if (
+      !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email
+      )
+    ) {
+      console.log("ERROR: email invalid");
+      return null;
     }
-}
-
-exports.update = async function(user_id, firstName, lastName, email, password, currentPassword) {
-
-    const queryString = 'UPDATE user SET ? WHERE id = ?';
 
     const values = [
-        firstName,
-        lastName,
-        email,
-        await passwordHelper.hashPassword(password)
+      firstName,
+      lastName,
+      email,
+      await passwordHelper.hashPassword(password),
     ];
 
-    await db.getPool().query(queryString, values);
-}
+    const result = await db.getPool().query(queryString, values);
+    console.log(result);
+    return result[0].insertId;
+  } catch (err) {
+    console.log("ERROR: Duplicate email");
+    return null;
+  }
+};
 
+exports.update = async function (
+  user_id,
+  firstName,
+  lastName,
+  email,
+  password
+) {
+  const queryString = "UPDATE user SET ? WHERE id = ?";
 
-exports.getUserById = async function(userId) {
-    const queryString = 'SELECT first_name, last_name, email FROM user WHERE id = ?';
+  const values = [
+    firstName,
+    lastName,
+    email,
+    await passwordHelper.hashPassword(password),
+  ];
 
-    try {
-        const [result] = await db.getPool().query(queryString, [userId]);
-        console.log(result[0]);
-        return {
-            firstName: result[0].first_name,
-            lastName: result[0].last_name,
-            email: result[0].email
-        };
-    } catch (err) {
-        return null;
+  await db.getPool().query(queryString, values);
+};
+
+exports.getUserById = async function (userId, currentUser = false) {
+  const queryString =
+    "SELECT first_name, last_name, email FROM user WHERE id = ?";
+
+  try {
+    const [result] = await db.getPool().query(queryString, [userId]);
+    console.log(result[0]);
+    if (currentUser) {
+      return {
+        firstName: result[0].first_name,
+        lastName: result[0].last_name,
+        email: result[0].email,
+      };
+    } else {
+      return {
+        firstName: result[0].first_name,
+        lastName: result[0].last_name,
+      };
     }
-    // const conn = await db.getPool().getConnection();
-    // const result = await conn.query(queryString, [user_id]);
-    // conn.release();
-    return result;
-}
+  } catch (err) {
+    return null;
+  }
+  // const conn = await db.getPool().getConnection();
+  // const result = await conn.query(queryString, [user_id]);
+  // conn.release();
+  return result;
+};
 
-
-exports.findByEmail = async function(email) {
-    const queryString = 'SELECT id, email, first_name, last_name, password FROM user WHERE email = ?';
-    // console.log(email);
-    try {
-        const result = await db.getPool().query(queryString, [email]);
-        if (result.length < 1) {
-            return null;
-        } else {
-            // console.log(result)
-            return result[0];
-        }
-    } catch (err) {
-        console.log("ERROR: findByEmail");
-        return null;
+exports.findByEmail = async function (email) {
+  const queryString =
+    "SELECT id, email, first_name, last_name, password FROM user WHERE email = ?";
+  // console.log(email);
+  try {
+    const result = await db.getPool().query(queryString, [email]);
+    if (result.length < 1) {
+      return null;
+    } else {
+      // console.log(result)
+      return result[0];
     }
-}
+  } catch (err) {
+    console.log("ERROR: findByEmail");
+    return null;
+  }
+};
 
+exports.setAuthToken = async function (userId) {
+  const queryString = "UPDATE user SET auth_token = ? WHERE id = ?";
 
-exports.setAuthToken = async function(userId) {
+  const auth_token = tokenGenerator.generate(32);
+  const values = [auth_token, userId];
 
-    const queryString = 'UPDATE user SET auth_token = ? WHERE id = ?';
+  try {
+    const result = await db.getPool().query(queryString, values);
+    console.log(result);
+    return [result, auth_token];
+  } catch (err) {
+    return null;
+  }
+};
 
-    const auth_token = tokenGenerator.generate(32);
-    const values = [
-        auth_token,
-        userId
-    ];
+exports.findByToken = async function (authToken) {
+  const queryString = "SELECT id FROM user WHERE auth_token = ?";
 
-    try {
-        const result = await db.getPool().query(queryString, values);
-        console.log(result);
-        return [result, auth_token];
-    } catch (err) {
-        return null;
-    }
-}
-
-
-exports.findByToken = async function(authToken) {
-    const queryString = "SELECT id FROM user WHERE auth_token = ?";
-
-    try {
-        const authorizedUserId = await db.getPool().query(queryString, authToken);
-        // console.log("========================================================");
-        // console.log(JSON.stringify(authorizedUserId));
-        // console.log("========================================================");
-        // console.log(authorizedUserId[0][0].id);
-        return authorizedUserId[0][0].id;
-    } catch (err) {
-        return null;
-    }
-}
-
-
-exports.logout = async function(authorizedUserId) {
+  try {
+    const authorizedUserId = await db.getPool().query(queryString, authToken);
     // console.log("========================================================");
-    // console.log(authorizedUserId);
+    // console.log(JSON.stringify(authorizedUserId));
     // console.log("========================================================");
+    // console.log(authorizedUserId[0][0].id);
+    return authorizedUserId[0][0].id;
+  } catch (err) {
+    return null;
+  }
+};
 
-    const queryString = "UPDATE user SET auth_token = NULL WHERE id = ?";
-    await db.getPool().query(queryString, authorizedUserId);
-}
+exports.logout = async function (authorizedUserId) {
+  // console.log("========================================================");
+  // console.log(authorizedUserId);
+  // console.log("========================================================");
+
+  const queryString = "UPDATE user SET auth_token = NULL WHERE id = ?";
+  await db.getPool().query(queryString, authorizedUserId);
+};
