74cc6f59808d11b6a58abeb28ceba372de184754
Vinnie <vjj14@uclive.ac.nz>
2021-03-30 21:02:31 +1300

    Testing API test server

--- app/middleware/password.middleware.js
+++ app/middleware/password.middleware.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcrypt');
 
 exports.hashPassword = async function(password) {
     const saltRounds = 10;
-    return await bcrypt.hash(password, saltRounds);
+    return await bcrypt.hash(password.toString(), saltRounds);
 }
 
 exports.compare = async function(password, hash) {
