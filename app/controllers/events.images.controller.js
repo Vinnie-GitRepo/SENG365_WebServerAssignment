e672c381352262729c2de29b2527a757ad742cb7
Vinnie <vjj14@uclive.ac.nz>
2021-04-01 05:36:17 +1300

    Testing API test server

--- app/controllers/events.images.controller.js
+++ app/controllers/events.images.controller.js
@@ -7,5 +7,14 @@ exports.read = async function(req, res) {
 
 exports.update = async function(req, res) {
     console.log("\nRequest to set an event's hero image...");
-    res.status(500).send();
+
+    const token = req.headers["x-authorization"];
+    if (!token) {
+        res.status(401);
+    }
+
+    const modifyingSelf = await user.findByToken(token);
+    if (!modifyingSelf) {
+        res.status(401).send();
+    }
 };
